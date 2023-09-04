59f0f1c2a2ddbff4222804bb55d01be2e0fd1155
Dominik Paľo <dominik.palo@student.tuke.sk>
2023-09-04 17:47:12 +0200

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import React from "react";
+
 import Demo from "./components/Demo";
 import Hero from "./components/Hero";
-
+import './App.css'
 const App = () => {
   return (
     <main>
       <div className="main">
         <div className="gradient" />
-
+        </div>
         <div className="app">
           <Hero />
           <Demo />
         </div>
-      </div>
+     
     </main>
   );
 };
--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
-
+import { useState } from "react";
+import linkIcon from "../assets/link.svg";
 const Demo = () => {
-  return (
-    <div>Demo</div>
-  )
-}
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
 
-export default Demo
+  const handleSubmit = async (e) => {
+   
+  };
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute ml-3 w-5 left-0 my-2"
+          />
+
+          <input
+            type="url"
+            placeholder="Enter URL"
+            value={article.url}
+            onChange={(e) =>
+              setArticle({
+                ...article,
+                url: e.target.value,
+              })
+            }
+            required
+            className="url_input peer"
+          />
+          <button
+            type="submit"
+            className="submit_btn peer-focus: border-gray-700 peer-focus:text-gray-700"
+          >
+            <p>↵</p>
+          </button>
+        </form>
+        {/* History */}
+      </div>
+      {/* Results */}
+    </section>
+  );
+};
+
+export default Demo;
