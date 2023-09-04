feec6c4c280834a0b4706dac5f2feda07495707b
Dominik Paľo <dominik.palo@student.tuke.sk>
2023-09-04 14:23:17 +0200

    init my project to sumarrize docs

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Demo from "./components/Demo";
+import Hero from "./components/Hero";
+
+const App = () => {
+  return (
+    <main>
+      <div className="main">
+        <div className="gradient" />
+
+        <div className="app">
+          <Hero />
+          <Demo />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default App;
